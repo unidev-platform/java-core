727fbae21538b4da1278075826c9571f6878da41
Denis O <denis.o@linux.com>
2016-12-06 10:39:50 +0200

    Added holder, list of messages

---
+++ unidev-dto/src/main/java/com/unidev/platform/common/dto/message/MessageList.java
@@ -0,0 +1,10 @@
+package com.unidev.platform.common.dto.message;
+
+import java.util.ArrayList;
+
+/**
+ * Holder for list of messages
+ * @param <T>
+ */
+public class MessageList<T extends Message> extends ArrayList<T> {
+}
