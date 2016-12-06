c315b562b92a0fbf7a4e38a07326bde57b1718e1
Denis O <denis.o@linux.com>
2016-12-06 10:51:56 +0200

    Added runtime exception

---
+++ unidev-exception/src/main/java/com/unidev/platform/common/exception/UnidevRuntimeException.java
@@ -0,0 +1,20 @@
+package com.unidev.platform.common.exception;
+
+/**
+ * Generic runtime exception object
+ */
+public class UnidevRuntimeException extends RuntimeException {
+
+    public UnidevRuntimeException() {
+        super();
+    }
+    public UnidevRuntimeException(String message) {
+        super(message);
+    }
+    public UnidevRuntimeException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    public UnidevRuntimeException(Throwable cause) {
+        super(cause);
+    }
+}
