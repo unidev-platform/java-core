f9c9c972c69b3079f0cda69358207581a1572c98
Denis O <denis.o@linux.com>
2016-12-30 11:08:04 +0200

    API method for removal of strings between passed markers

--- unidev-strings/src/main/java/com/unidev/platform/Strings.java
+++ unidev-strings/src/main/java/com/unidev/platform/Strings.java
@@ -2,7 +2,6 @@ package com.unidev.platform;
 
 import org.apache.commons.lang3.StringUtils;
 
-import java.security.SecureRandom;
 import java.util.Random;
 
 /**
@@ -10,6 +9,25 @@ import java.util.Random;
  * @author denis
  */
 public class Strings  extends StringUtils{
+
+    /**
+     * Remove first value between begin end, if not pattern not found return null
+     * @param content Content which should be updated
+     * @param begin Begin marker
+     * @param end  End marker
+     * @param includeBeginEnd Include in removal begin end
+     * @return Updated content with removed value
+     */
+    public static String removeValueBetween(String content, String begin, String end, boolean includeBeginEnd) {
+        String valueBetween = substringBetween(content, begin, end);
+        if (isBlank(valueBetween)) {
+            return null;
+        }
+        if (includeBeginEnd) {
+            return replaceOnce(content, begin + valueBetween + end, "");
+        }
+        return replaceOnce(content, valueBetween, "");
+    }
 
     /**
      * Generate string unique hash based on provided dictionary
@@ -21,7 +39,7 @@ public class Strings  extends StringUtils{
      */
     public static String genHash(String str, int minLength, int maxLength, String dictionary) {
         StringBuilder stringBuilder = new StringBuilder();
-        Random random = new SecureRandom(str.getBytes());
+        Random random = new Random(str.hashCode());
         int length = minLength + random.nextInt(maxLength - minLength);
         for (int i = 0; i < length; i++) {
             int id = random.nextInt(dictionary.length());
