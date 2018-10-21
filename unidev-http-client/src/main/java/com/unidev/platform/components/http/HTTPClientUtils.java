8d109a1fab6aedbfff2bfbb7dd8dbc7abc2847cf
Denis O <denis@universal-development.com>
2018-10-21 21:57:57 +0300

    3.0.4-S

---
+++ unidev-http-client/src/main/java/com/unidev/platform/components/http/HTTPClientUtils.java
@@ -0,0 +1,9 @@
+package com.unidev.platform.components.http;
+
+/**
+ * Compatibility class for unidev-platform 2
+ */
+@Deprecated
+public class HTTPClientUtils extends com.unidev.platform.http.HTTPClientUtils {
+
+}
