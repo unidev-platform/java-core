5db9cc72ef38392fd14b116d81d0def2121b8e03
Denis O <denis@universal-development.com>
2018-10-21 21:52:16 +0300

    Added compatibility classes

---
+++ unidev-common/src/main/java/com/unidev/platform/common/utils/RandomUtils.java
@@ -0,0 +1,11 @@
+package com.unidev.platform.common.utils;
+
+import com.unidev.platform.Randoms;
+
+/**
+ * Compatibility class from unidev-platform 2.0
+ **/
+@Deprecated
+public class RandomUtils extends Randoms {
+
+}
---
+++ unidev-common/src/main/java/com/unidev/platform/common/utils/StringUtils.java
@@ -0,0 +1,11 @@
+package com.unidev.platform.common.utils;
+
+import com.unidev.platform.Strings;
+
+/**
+ * Compatibility class from unidev-platform 2.0
+ */
+@Deprecated
+public class StringUtils extends Strings {
+
+}
