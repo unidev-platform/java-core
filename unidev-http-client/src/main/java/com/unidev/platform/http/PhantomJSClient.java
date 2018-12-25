de652bd7e49a5b998f8a527d344920ac54315ba1
Denis O <denis@universal-development.com>
2018-12-25 21:37:36 +0200

    Phantomjs script removal

--- unidev-http-client/src/main/java/com/unidev/platform/http/PhantomJSClient.java
+++ unidev-http-client/src/main/java/com/unidev/platform/http/PhantomJSClient.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright (c) 2018 Denis O <denis.o@linux.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.unidev.platform.http;
 
 import com.unidev.platform.Processes;
@@ -14,7 +29,6 @@ import java.nio.charset.Charset;
 /**
  * PhantomJS based client
  */
-
 @NoArgsConstructor
 @AllArgsConstructor
 public class PhantomJSClient {
@@ -28,9 +42,15 @@ public class PhantomJSClient {
     private Processes processes;
 
     public String runScript(String script) throws Exception {
-        File scriptFile = File.createTempFile("phantomjs", ".js");
-        FileUtils.write(scriptFile, script, Charset.defaultCharset());
-        ProcessToRun processToRun = ProcessToRun.builder().command(phantomBinary + " " + scriptFile.getAbsolutePath()).build();
-        return processes.runProcess(processToRun);
+        File scriptFile = null;
+        try {
+            scriptFile = File.createTempFile("phantomjs", ".js");
+            scriptFile.deleteOnExit();
+            FileUtils.write(scriptFile, script, Charset.defaultCharset());
+            ProcessToRun processToRun = ProcessToRun.builder().command(phantomBinary + " " + scriptFile.getAbsolutePath()).build();
+            return processes.runProcess(processToRun);
+        } finally {
+            FileUtils.deleteQuietly(scriptFile);
+        }
     }
 }
