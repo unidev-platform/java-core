13cad2671a40924e107d6036fa5fa421bf9428db
Denis O <denis@universal-development.com>
2019-03-24 11:57:42 +0200

    Statistics manager

---
+++ unidev-common/src/main/java/com/unidev/platform/StatisticsManager.java
@@ -0,0 +1,41 @@
+package com.unidev.platform;
+
+import com.unidev.platform.model.StatItem;
+import lombok.Getter;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class StatisticsManager {
+
+    @Getter
+    private Map<String, StatItem> statistics = new ConcurrentHashMap<>();
+
+    public void add(String key) {
+        add(key, 1L);
+    }
+
+    public void add(String key, long value) {
+        createIfMissing(key).add(value);
+    }
+
+    public StatItem fetchStat(String key) {
+        return createIfMissing(key);
+    }
+
+    private StatItem createIfMissing(String key) {
+        statistics.putIfAbsent(key, StatItem.builder().build());
+        return statistics.get(key);
+    }
+
+    public String toString() {
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append("Statistics: \n");
+        statistics.entrySet().forEach(item -> {
+            stringBuilder.append(item.getKey() + " : " + item.getValue() + "\n");
+        });
+
+        return stringBuilder.toString();
+    }
+
+}
---
+++ unidev-common/src/main/java/com/unidev/platform/model/StatItem.java
@@ -0,0 +1,27 @@
+package com.unidev.platform.model;
+
+import lombok.Builder;
+import lombok.Data;
+
+/**
+ * Item to store statistics
+ */
+@Data
+@Builder
+public class StatItem {
+
+    private long total;
+    private long count;
+    private long lastValue;
+
+    public void add(long value) {
+        lastValue = value;
+        total += value;
+        count++;
+    }
+
+    public float average() {
+        return total / (float) count;
+    }
+
+}
