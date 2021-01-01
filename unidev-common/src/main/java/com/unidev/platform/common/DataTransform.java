19113ce0922d832de6e630938f075a1fa13d8274
Denis O <denis@universal-development.com>
2021-01-01 11:32:46 +0200

    Migration of data processing

---
+++ unidev-common/src/main/java/com/unidev/platform/common/DataTransform.java
@@ -0,0 +1,37 @@
+package com.unidev.platform.common;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Map;
+import java.util.Optional;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+
+@AllArgsConstructor
+@Slf4j
+public class DataTransform {
+
+  @Getter
+  @Setter
+  private final ObjectMapper objectMapper;
+
+  public Optional<Map> toMap(Object object) {
+    try {
+      return Optional.ofNullable(objectMapper.readValue(objectMapper.writeValueAsBytes(object), Map.class));
+    } catch (Exception e) {
+      log.error("Failed to create map", e);
+      return Optional.empty();
+    }
+  }
+
+  public <T> Optional<T> toObject(Object object, Class<T> clazz) {
+    try {
+      return Optional.ofNullable(objectMapper.readValue(objectMapper.writeValueAsBytes(object), clazz));
+    } catch (Exception e) {
+      log.error("Failed to transform object", e);
+      return Optional.empty();
+    }
+  }
+
+}
